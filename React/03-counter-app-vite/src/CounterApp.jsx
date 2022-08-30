b72b3437949740eb190f18a65ee32ee322dcbc57
Dary <dary.cc@gmail.com>
2022-08-30 03:35:50 -0500

    Hooks,fin a introducción a React

--- React/03-counter-app-vite/src/CounterApp.jsx
+++ React/03-counter-app-vite/src/CounterApp.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {fragment} from 'react';
+import React,{useState} from 'react';
+// import ReactDOM from 'react-dom/client';
+// import {fragment} from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value})=>{
-  console.log(value);
+  // console.log(value);
+  const [counter, setCounter] = useState(value);
+
+  const incrementaContador=()=> {
+    return setCounter(()=> counter+1);
+  }
+  const decrementarContador =()=> {
+    setCounter(counter-1);
+  }
+  const resetCounter = ()=>{
+    setCounter(value);
+  };
+
   return (
     <>
    <h1>CounterApp</h1>
-   <h2>{value}</h2>
+   <h2> {counter} </h2>
+      <button onClick={incrementaContador} name=''>+1</button>
+      <button onClick={decrementarContador}>-1</button>
+      <button onClick={resetCounter}>Reset</button>
     </>
   );
 };
--- React/03-counter-app-vite/src/main.jsx
+++ React/03-counter-app-vite/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import PropTypes from 'prop-types';
-import Hello,{Dary} from './HelloWorldApp';
-import {FirstApp  } from './FirstApp';
+// import Hello,{Dary} from './HelloWorldApp';
+// import {FirstApp  } from './FirstApp';
 import {CounterApp} from './CounterApp';
 
 import './style.css'
